01eb39321bdf0a2131e54c7ef221162e7e274d51
kart7990 <holland.david90@gmail.com>
2023-12-21 09:56:31 -0600

    abstract session connection from dashboard

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,158 +1,16 @@
 "use client"
-import { useState, useEffect, useRef, createRef } from 'react'
 import { Card, Grid, Tab, TabGroup, Metric, Flex, ProgressBar, TabList, TabPanel, TabPanels, Text, Title, } from "@tremor/react"
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import axios from 'axios'
 import GridLayout, { ItemCallback } from "react-grid-layout"
-import { setIntervalAsync } from 'set-interval-async/dynamic'
-import { clearIntervalAsync } from 'set-interval-async'
 
-import {
-    standingsSlice,
-    useDispatch
-} from '@/lib/redux'
-
-import { API_BASE_URL, API_V1_URL } from "@/config/domain.config"
+import PitwallSession from '@/app/components/connection/pitwallSession';
 import { Standings } from '@/app/components/standings/standings';
 import { TrackMap } from '@/app/components/trackmap/trackmap';
 
-let sessionDynamicDataLastResponse = 1
-let standingsDataLastResponse = 1
-let _lapsLastUpdate = -1
-let _lapsLastTelemetryLap = -1
-let _isCarTelemetryActive = false
+
 
 export default function Page({ params }: { params: { id: string } }) {
-    const dispatch = useDispatch()
     const pitboxSessionId = params.id
     console.log(pitboxSessionId)
-
-    //Web-Socket Connections
-    const [sessionConnection, setSessionConnection] = useState<HubConnection>();
-    const [standingsConnection, setStandingsConnection] = useState<HubConnection>();
-
-    //Session state
-    const [joinSessionLastLapSessionTime, setJoinSessionLastLapSessionTime] = useState(-1);
-    const [joinSessionLastTelemetryLap, setJoinSessionLastTelemetryLap] = useState(-1);
-
-    // #region Session Join Request
-    useEffect(() => {
-        const joinSesion = async () => {
-            var joinSessionResponse = await axios.get(`${API_V1_URL}/pitbox/session/${pitboxSessionId}`);
-
-            if (joinSessionResponse.data.completedLaps.length > 0) {
-                const [lastLap] = joinSessionResponse.data.completedLaps.slice(-1)
-                setJoinSessionLastLapSessionTime(lastLap.sessionTimeLapEnd)
-            } else {
-                setJoinSessionLastLapSessionTime(0)
-            }
-
-            if (joinSessionResponse.data.telemetryLaps.length > 0) {
-                const [lastLap] = joinSessionResponse.data.telemetryLaps.slice(-1)
-                setJoinSessionLastTelemetryLap(lastLap.lapNumber)
-            } else {
-                setJoinSessionLastTelemetryLap(0)
-            }
-
-
-            var sessionConnection = buildHubConnection(joinSessionResponse.data.webSocketEndpoints.Session, joinSessionResponse.data.pitBoxSession.id)
-            var standingsConnection = buildHubConnection(joinSessionResponse.data.webSocketEndpoints.Standings, joinSessionResponse.data.pitBoxSession.id);
-
-            await sessionConnection.start()
-            await standingsConnection.start()
-            setSessionConnection(sessionConnection)
-            setStandingsConnection(standingsConnection)
-        }
-        joinSesion();
-    }, []);
-
-
-    const buildHubConnection = (socketEndpoint: string, sessionId: string) => {
-        const options = {
-            //accessTokenFactory: () => auth.getAccessToken()
-        };
-        return new HubConnectionBuilder()
-            .withUrl(API_BASE_URL + socketEndpoint + "?sessionId=" + sessionId, options)
-            .withAutomaticReconnect()
-            .build()
-    }
-    // #endregion
-
-    // #region Session WebSocket Connection
-    useEffect(() => {
-        if (sessionConnection) {
-            const connect = async () => {
-                sessionConnection.on('onSessionReset', pitboxSession => {
-                    //console.log('onSessionReset', pitboxSession)
-                    _lapsLastUpdate = 0
-                    _lapsLastTelemetryLap = 0
-                })
-                sessionConnection.on('onTrackSessionChanged', trackSession => {
-                    //console.log('trackSession', trackSession)
-                    _lapsLastUpdate = 0
-                    _lapsLastTelemetryLap = 0
-                })
-                sessionConnection.on('onDynamicSessionDataUpdate', dynamicSessionData => {
-                    //console.log('dynamicSessionData', dynamicSessionData)
-                    if (dynamicSessionData?.timing?.simTimeOfDay) {
-                        var seconds = dynamicSessionData.timing.simTimeOfDay; // Some arbitrary value
-                        var date = new Date(seconds * 1000); // multiply by 1000 because Date() requires miliseconds
-                        var timeStr = date.toISOString();
-                        dynamicSessionData.timing.simTimeOfDay = timeStr
-                    }
-                    _isCarTelemetryActive = dynamicSessionData.isCarTelemetryActive
-                    sessionDynamicDataLastResponse = Date.now()
-                })
-            }
-            connect()
-        }
-    }, [sessionConnection]);
-
-    useEffect(() => {
-        if (sessionConnection) {
-            var lastRequest1 = 0;
-            const timer = setIntervalAsync(
-                async () => {
-                    if ((sessionDynamicDataLastResponse > lastRequest1)) {
-                        lastRequest1 = Date.now();
-                        await sessionConnection.invoke("RequestDynamicSessionData", { sessionId: pitboxSessionId, teamId: "" });
-                    }
-                },
-                1000)
-            async () => await clearIntervalAsync(timer);
-        }
-    }, [sessionConnection]);
-
-    // #endregion
-
-    // #region Standings WebSocket Connection
-    useEffect(() => {
-        if (standingsConnection) {
-            const connect = async () => {
-                standingsConnection.on('onStandingsUpdate', standings => {
-                    //console.log('standings', standings)
-                    dispatch(standingsSlice.actions.update(standings))
-                    standingsDataLastResponse = Date.now()
-                })
-            }
-            connect()
-        }
-    }, [standingsConnection]);
-
-    useEffect(() => {
-        if (standingsConnection) {
-            var lastRequest = 0;
-            const timer = setIntervalAsync(
-                async () => {
-                    if ((standingsDataLastResponse > lastRequest)) {
-                        lastRequest = Date.now();
-                        await standingsConnection.invoke("RequestStandings", { sessionId: pitboxSessionId, teamId: "" });
-                    }
-                },
-                333)
-            async () => await clearIntervalAsync(timer);
-        }
-    }, [standingsConnection]);
 
     // #endregion
     const onResizeStopped: ItemCallback = (layout, oldItem, newItem) => {
@@ -160,60 +18,57 @@ export default function Page({ params }: { params: { id: string } }) {
     };
 
     return (
-        <main className="p-12">
-            <Title>Dashboard</Title>
-            <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+        <PitwallSession pitwallSessionId={pitboxSessionId}>
+            <main className="p-12">
+                <Title>Dashboard</Title>
+                <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
 
-            <TabGroup className="mt-6">
-                <TabList>
-                    <Tab>Overview</Tab>
-                    <Tab>Details</Tab>
-                </TabList>
-                <TabPanels>
-                    <TabPanel>
-                        <GridLayout className="layout" cols={12} rowHeight={30} width={4000} onResizeStop={onResizeStopped}>
-                            <div key="a" data-grid={{ x: 2, y: 0, w: 3, h: 15, minH: 6 }}>
-                                <Standings />
-                            </div>
-                            <div key="trackMap" data-grid={{ x: 0, y: 3, w: 2, h: 15, minH: 6 }}>
-                                <Card className="h-full overflow-hidden">
-                                    <TrackMap />
+                <TabGroup className="mt-6">
+                    <TabList>
+                        <Tab>Overview</Tab>
+                        <Tab>Details</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <GridLayout className="layout" cols={12} rowHeight={30} width={4000} onResizeStop={onResizeStopped}>
+                                <div key="a" data-grid={{ x: 2, y: 0, w: 3, h: 15, minH: 6 }}>
+                                    <Standings />
+                                </div>
+                                <div key="trackMap" data-grid={{ x: 0, y: 3, w: 2, h: 15, minH: 6 }}>
+                                    <Card className="h-full overflow-hidden">
+                                        <TrackMap />
+                                    </Card>
+                                </div>
+                                <div key="b" data-grid={{ x: 0, y: 5, w: 3, h: 2, minW: 2, maxW: 4 }}>
+                                    <Card className="h-full">
+                                        {/* <Logs logs={['abc123', 'asdad', 'asdasdas']} /> */}
+                                    </Card>
+                                </div>
+                            </GridLayout>
+                        </TabPanel>
+                        <TabPanel>
+                            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
+                                <Card>
+                                    {/* Placeholder to set height */}
+                                    <div className="h-28" />
+                                </Card>
+                                <Card>
+                                    {/* Placeholder to set height */}
+                                    <div className="h-28" />
+                                </Card>
+                                <Card className="max-w-xs mx-auto">
+
+                                </Card>
+                            </Grid>
+                            <div className="mt-6">
+                                <Card>
+                                    <div className="h-80" />
                                 </Card>
                             </div>
-                            <div key="b" data-grid={{ x: 0, y: 5, w: 3, h: 2, minW: 2, maxW: 4 }}>
-                                <Card className="h-full">
-                                </Card>
-                            </div>
-                        </GridLayout>
-                    </TabPanel>
-                    <TabPanel>
-                        <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-                            <Card>
-                                {/* Placeholder to set height */}
-                                <div className="h-28" />
-                            </Card>
-                            <Card>
-                                {/* Placeholder to set height */}
-                                <div className="h-28" />
-                            </Card>
-                            <Card className="max-w-xs mx-auto">
-                                <Text>Sales</Text>
-                                <Metric>$ 71,465</Metric>
-                                <Flex className="mt-4">
-                                    <Text>32% of annual target</Text>
-                                    <Text>$ 225,000</Text>
-                                </Flex>
-                                <ProgressBar value={32} className="mt-2" />
-                            </Card>
-                        </Grid>
-                        <div className="mt-6">
-                            <Card>
-                                <div className="h-80" />
-                            </Card>
-                        </div>
-                    </TabPanel>
-                </TabPanels>
-            </TabGroup>
-        </main>
+                        </TabPanel>
+                    </TabPanels>
+                </TabGroup>
+            </main>
+        </PitwallSession>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from 'next/image'
-import { Counter } from './components/Counter/Counter'
 
 export default function Home() {
   return (
@@ -29,7 +28,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <Counter/>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
