a12ff4736330ad2bc29ec171dcc95367829133e3
kart7990 <holland.david90@gmail.com>
2023-12-21 14:56:45 -0600

    remove tremor

--- src/app/components/standings/standings.tsx
+++ src/app/components/standings/standings.tsx
@@ -1,17 +1,5 @@
 'use client'
 import { StatusOnlineIcon } from "@heroicons/react/outline";
-import {
-    Badge,
-    Card,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeaderCell,
-    TableRow,
-    Text,
-    Title,
-} from "@tremor/react";
 
 import {
     useSelector,
@@ -21,35 +9,36 @@ import {
 export const Standings = () => {
     const standings = useSelector(selectLiveTimimg)
     return (
-        <Card className="h-full overflow-y-scroll">
-            <Title>Standings</Title>
-            <Table className="mt-5">
-                <TableHead>
-                    <TableRow>
-                        <TableHeaderCell>Name</TableHeaderCell>
-                        <TableHeaderCell>Position</TableHeaderCell>
-                        <TableHeaderCell>Department</TableHeaderCell>
-                        <TableHeaderCell>Status</TableHeaderCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {standings.map((item) => (
-                        <TableRow>
-                            <TableCell>{JSON.stringify(item)}</TableCell>
-                            <TableCell>
-                                <Text></Text>
-                            </TableCell>
-                            <TableCell>
-                                <Text></Text>
-                            </TableCell>
-                            <TableCell>
-                                <Badge color="emerald" icon={StatusOnlineIcon}>
-                                </Badge>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Card>
+        <></>
+        // <Card className="h-full overflow-y-scroll">
+        //     <Title>Standings</Title>
+        //     <Table className="mt-5">
+        //         <TableHead>
+        //             <TableRow>
+        //                 <TableHeaderCell>Name</TableHeaderCell>
+        //                 <TableHeaderCell>Position</TableHeaderCell>
+        //                 <TableHeaderCell>Department</TableHeaderCell>
+        //                 <TableHeaderCell>Status</TableHeaderCell>
+        //             </TableRow>
+        //         </TableHead>
+        //         <TableBody>
+        //             {standings.map((item) => (
+        //                 <TableRow>
+        //                     <TableCell>{JSON.stringify(item)}</TableCell>
+        //                     <TableCell>
+        //                         <Text></Text>
+        //                     </TableCell>
+        //                     <TableCell>
+        //                         <Text></Text>
+        //                     </TableCell>
+        //                     <TableCell>
+        //                         <Badge color="emerald" icon={StatusOnlineIcon}>
+        //                         </Badge>
+        //                     </TableCell>
+        //                 </TableRow>
+        //             ))}
+        //         </TableBody>
+        //     </Table>
+        // </Card>
     )
 }
--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { Card, Grid, Tab, TabGroup, Metric, Flex, ProgressBar, TabList, TabPanel, TabPanels, Text, Title, } from "@tremor/react"
 import GridLayout, { ItemCallback } from "react-grid-layout"
 
 import PitwallSession from '@/app/components/connection/pitwallSession';
@@ -20,54 +19,18 @@ export default function Page({ params }: { params: { id: string } }) {
     return (
         <PitwallSession pitwallSessionId={pitboxSessionId}>
             <main className="p-12">
-                <Title>Dashboard</Title>
-                <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
 
-                <TabGroup className="mt-6">
-                    <TabList>
-                        <Tab>Overview</Tab>
-                        <Tab>Details</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <GridLayout className="layout" cols={12} rowHeight={30} width={4000} onResizeStop={onResizeStopped}>
-                                <div key="standings" data-grid={{ x: 2, y: 0, w: 3, h: 15, minH: 6 }}>
-                                    <Standings />
-                                </div>
-                                <div key="trackMap" data-grid={{ x: 0, y: 3, w: 2, h: 15, minH: 6 }}>
-                                    <Card className="h-full overflow-hidden">
-                                        <TrackMap />
-                                    </Card>
-                                </div>
-                                <div key="b" data-grid={{ x: 0, y: 5, w: 3, h: 2, minW: 2, maxW: 4 }}>
-                                    <Card className="h-full">
-                                        {/* <Logs logs={['abc123', 'asdad', 'asdasdas']} /> */}
-                                    </Card>
-                                </div>
-                            </GridLayout>
-                        </TabPanel>
-                        <TabPanel>
-                            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-                                <Card>
-                                    {/* Placeholder to set height */}
-                                    <div className="h-28" />
-                                </Card>
-                                <Card>
-                                    {/* Placeholder to set height */}
-                                    <div className="h-28" />
-                                </Card>
-                                <Card className="max-w-xs mx-auto">
-
-                                </Card>
-                            </Grid>
-                            <div className="mt-6">
-                                <Card>
-                                    <div className="h-80" />
-                                </Card>
-                            </div>
-                        </TabPanel>
-                    </TabPanels>
-                </TabGroup>
+                <GridLayout className="layout" cols={12} rowHeight={30} width={4000} onResizeStop={onResizeStopped}>
+                    <div key="standings" data-grid={{ x: 2, y: 0, w: 3, h: 15, minH: 6 }}>
+                        <Standings />
+                    </div>
+                    <div key="trackMap" data-grid={{ x: 0, y: 3, w: 2, h: 15, minH: 6 }}>
+                            <TrackMap />
+                    </div>
+                    <div key="b" data-grid={{ x: 0, y: 5, w: 3, h: 2, minW: 2, maxW: 4 }}>
+                            {/* <Logs logs={['abc123', 'asdad', 'asdasdas']} /> */}
+                    </div>
+                </GridLayout>
             </main>
         </PitwallSession>
     )
