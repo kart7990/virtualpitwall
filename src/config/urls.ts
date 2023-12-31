1f46bfee7da91573316c8aef3b0c44311ce5e6fb
kart7990 <holland.david90@gmail.com>
2023-12-31 11:53:05 -0600

    add localserver env

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = process.env.BASE_URL
-export const API_BASE_URL = (process.env.APP_ENV == 'production' || process.env.APP_ENV == 'staging') ? process.env.BASE_URL + '/api' : BASE_URL
+export const API_BASE_URL = (process.env.APP_ENV != 'development-localserver') ? process.env.BASE_URL + '/api' : BASE_URL
 export const API_V1_URL = API_BASE_URL + '/v1.0'
 export const API_V2_URL = API_BASE_URL + '/v2.0'
