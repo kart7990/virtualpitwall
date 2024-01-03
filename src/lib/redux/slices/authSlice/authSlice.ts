cf3618e4d7f52df0277f2d824ae2766c892abea8
kart7990 <holland.david90@gmail.com>
2024-01-02 20:43:12 -0600

    add logoff

--- src/lib/redux/slices/authSlice/authSlice.ts
+++ src/lib/redux/slices/authSlice/authSlice.ts
@@ -9,11 +9,11 @@ export interface JWT {
 }
 
 export interface AuthSliceState {
-    oAuthToken?: JWT
+    oAuthToken: JWT | null
 }
 
 const initialState: AuthSliceState = {
-    oAuthToken: (typeof window !== 'undefined' && window.localStorage) ? localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)!!) : undefined : undefined
+    oAuthToken: (typeof window !== 'undefined' && window.localStorage) ? localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)!!) : null : null
 }
 
 export const authSlice = createSlice({
@@ -26,7 +26,7 @@ export const authSlice = createSlice({
         },
         logoff: (state) => {
             localStorage.removeItem(STORAGE_KEY)
-            state = { oAuthToken: undefined }
+            state.oAuthToken = null
         }
     }
 })
