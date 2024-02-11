0fc9046df371cd072c5103ed2a06628cc40ada0e
kart7990 <holland.david90@gmail.com>
2024-02-11 11:35:51 -0600

    get oauth client id from env config

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
   return (
     <div className="relative flex h-screen flex-col">
       <SiteHeader isPublic={true} />
-      <GoogleOAuthProvider clientId="931083495990-o00suecncvj0268ff3lnblcu0q984olu.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={process.env.GOOGLE_OAUTH_CLIENT_ID!!}>
         <div className="flex h-full border-b">{children}</div>
       </GoogleOAuthProvider>
       <SiteFooter />
