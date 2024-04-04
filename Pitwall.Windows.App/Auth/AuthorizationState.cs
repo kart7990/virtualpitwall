ffae6f38fdcf07333c815ae365015b4f66e821f1
kart7990 <holland.david90@gmail.com>
2024-04-03 22:29:51 -0500

    add auth

---
+++ Pitwall.Windows.App/Auth/AuthorizationState.cs
@@ -0,0 +1,23 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+
+namespace Pitwall.Windows.App.Auth
+{
+    public class AuthorizationState
+    {
+        public static string AccessToken { get; set; }
+        public static bool IsLoggedIn { get => !string.IsNullOrEmpty(AccessToken); }
+        public static IDictionary<string, object> Claims
+        {
+            get => JwtBuilder.Create().WithAlgorithm(new HMACSHA256Algorithm()).Decode<IDictionary<string, object>>(AccessToken);
+        }
+        public static string Name
+        {
+            get => Claims["name"].ToString();
+        }
+        public static void Logout()
+        {
+            AccessToken = null;
+        }
+    }
+}
